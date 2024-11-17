a0cd6b07bbd585a963b6c29bbd005310c130a394
Yuriy Syrovetskiy <git-orbi@cblp.su>
2024-11-17 14:36:23 +0100

    Add stack config; Remove unused dependencies; Autoformat cabal and haskell files

--- app/Codeforces/API.hs
+++ app/Codeforces/API.hs
@@ -5,23 +5,23 @@ module Codeforces.API where
 import Codeforces.Types
 import Control.Monad.IO.Class
 import Data.Aeson
-import qualified Data.Aeson.KeyMap as Km
-import qualified Data.Vector as V
+import Data.Aeson.KeyMap qualified as Km
+import Data.Vector qualified as V
 import Network.HTTP.Req
 
 userInfo :: String -> IO User
 userInfo usr = runReq defaultHttpConfig $ do
-  r <-
-    req
-      GET
-      (http "codeforces.com" /: "api" /: "user.info")
-      NoReqBody
-      jsonResponse
-      (queryParam "handles" $ Just usr)
-  let user = checkUser usr $ fromJSON $ V.head $ valToArr $ checkObj $ Km.lookup "result" (responseBody r :: Object)
-  liftIO $ return user
+    r <-
+        req
+            GET
+            (http "codeforces.com" /: "api" /: "user.info")
+            NoReqBody
+            jsonResponse
+            (queryParam "handles" $ Just usr)
+    let user = checkUser usr $ fromJSON $ V.head $ valToArr $ checkObj $ Km.lookup "result" (responseBody r :: Object)
+    liftIO $ return user
   where
-    checkUser h (Error s) = User {handle = h, rating = 0}
+    checkUser h (Error _) = User{handle = h, rating = 0}
     checkUser _ (Success a) = a
     checkObj :: Maybe Value -> Value
     checkObj Nothing = Null
